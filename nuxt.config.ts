567a78e6d103c77e37fff822fbf3be45a8978a09
sahnfatih <sahnfatih49@gmail.com>
2024-11-06 10:38:09 +0300

    +dosya ve sayfa düzeni

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  css: [
+    '@fortawesome/fontawesome-free/css/all.min.css',
+    '~/assets/styles/main.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/styles/base/_variables.scss";
+          `
+        }
+      }
+    }
+  }
 })
